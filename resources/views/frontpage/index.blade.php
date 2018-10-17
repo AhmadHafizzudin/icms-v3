f4fd8f35cc45d00094423e8cfc2188271d17fb91
soo.park.yu(din) <apispenn21@gmail.com>
2018-10-17 16:10:54 +0800

    Initial commit

---
+++ resources/views/frontpage/index.blade.php
@@ -0,0 +1,386 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Meta, title, CSS, favicons, etc. -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>LAMAN UTAMA </title>
+	
+	<!-- Styles -->
+	<link rel='stylesheet' href='frontpage/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='frontpage/css/animate.min.css'>
+	<link rel='stylesheet' href="frontpage/css/font-awesome.min.css"/>
+	<link rel='stylesheet' href="frontpage/css/style.css"/>
+	 <link rel="apple-touch-icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="images/favicon.png"> 
+
+
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+			
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="#">
+</head>
+<body>
+<!-- Begin Hero Bg -->
+<div id="parallax">
+</div>
+<!-- End Hero Bg
+	================================================== -->
+<!-- Start Header
+	================================================== -->
+<header id="header" class="navbar navbar-inverse navbar-fixed-top" role="banner">
+<div class="container">
+	<div class="navbar-header">
+		<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+		<!-- Your Logo -->
+		<a href="#hero" class="navbar-brand">INTEGRATED CAMPUS <br><span class="lighter">MANAGEMENT SYSTEM</span></a>
+	</div>
+	<!-- Start Navigation -->
+	<nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+	<ul class="nav navbar-nav">
+		<li>
+		<a href="#hero">Home</a>
+		</li>
+		<!-- <li>
+		<a href="#about">About</a>
+		</li> -->
+		<li>
+		<a href="#gallery">Gallery</a>
+		</li>
+		<!-- <li>
+		<a href="#slider">Testimonials</a>
+		</li>
+		<li>
+		<a href="#faq">FAQ</a>
+		</li> -->
+		<li>
+		<a href="#admin">Daftar Masuk <br>(Admin)</a>
+		</li>
+		<li>
+		<a href="#student">Daftar Masuk<br>(Pelatih)</a>
+		</li>
+		<li>
+		<a href="#contactarea">Contact</a>
+		</li>
+	</ul>
+	</nav>
+</div>
+</header>
+
+<!-- Intro
+	================================================== -->
+<section id="hero" class="section">
+<div class="container">
+	<div class="row">
+		<div class="col-md-5">
+			<div class="herotext">
+				<h1 class="wow bounceInDown" data-wow-duration="1s" data-wow-delay="0.1s">RUN <span class="lighter">CHARITY</span></h1>
+				<p class="lead wow zoomIn" data-wow-duration="2s" data-wow-delay="0.5s">
+					When we give cheerfully and accept gratefully, everyone is blessed.
+				</p>
+				<p>
+					<a href="#" class="btn btn-default btn-lg wow fadeInLeft" role="button"> View Gallery </a> &nbsp; <a href="#" class="btn btn-default btn-lg wow fadeInRight" role="button">Find a Cause</a>
+				</p>
+			</div>
+		</div>
+		<div class="col-md-7">
+		</div>
+	</div>
+</div>
+</section>
+
+
+<!-- Gallery
+	================================================== -->
+<section id="gallery" class="parallax section" style="background-image: url(http://themepush.com/demo/runcharity/frontpage/img/2.jpg);">
+<div class="wrapsection">
+	<div class="parallax-overlay" style="background-color:#00c1c1;opacity:0.9;"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 sol-sm-12">
+				<div class="maintitle">
+					<h3 class="section-title">Events Gallery</h3>
+					<p class="lead wow flipInX">
+						Have a look at your past events gallery, see how much fun we have while helping people all over the world. Join our community today!
+					</p>
+				</div>
+			</div>
+			
+			<div class="col-md-4">
+				<a href="https://ununsplash.imgix.net/photo-1429277158984-614d155e0017?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+					<img src="https://ununsplash.imgix.net/photo-1429277158984-614d155e0017?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">This is <b>awesome</b></span> 
+						<span class="camera"><i class="fa fa-camera"></i></span>
+						<div class="clearfix"></div>
+					</div>			
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="https://unsplash.imgix.net/uploads/14115408840644deb16b0/2dc933e3?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+				<img src="https://unsplash.imgix.net/uploads/14115408840644deb16b0/2dc933e3?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">Great <b>marathon</b></span> 
+						<span class="camera"><i class="fa fa-link"></i></span>
+						<div class="clearfix"></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="https://ununsplash.imgix.net/photo-1429277096327-11ee3b761c93?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+				<img src="https://ununsplash.imgix.net/photo-1429277096327-11ee3b761c93?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">Running <b>miles</b></span> 
+						<span class="camera"><i class="fa fa-heart"></i></span>
+						<div class="clearfix"></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="https://unsplash.imgix.net/uploads/141327328038701afeede/eda0fb7c?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+				<img src="https://unsplash.imgix.net/uploads/141327328038701afeede/eda0fb7c?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">Young <b>volunteers</b></span> 
+						<span class="camera"><i class="fa fa-tachometer"></i></span>
+						<div class="clearfix"></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="https://ununsplash.imgix.net/photo-1418225043143-90858d2301b4?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+				<img src="https://ununsplash.imgix.net/photo-1418225043143-90858d2301b4?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">Charity <b>concerts</b></span> 
+						<span class="camera"><i class="fa fa-microphone"></i></span>
+						<div class="clearfix"></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="https://unsplash.imgix.net/photo-1428908799722-0a74e26ce7f6?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" title="This is the description">
+				<img src="https://unsplash.imgix.net/photo-1428908799722-0a74e26ce7f6?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050" alt="" class="image-responsive">
+					<div class="description">
+						<span class="caption">Generous <b>sponsors</b></span> 
+						<span class="camera"><i class="fa fa-coffee"></i></span>
+						<div class="clearfix"></div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+
+<!-- <!- admin login
+	================================================== -->
+<section id="admin" class="parallax section" style="background-image: url(http://themepush.com/demo/runcharity/frontpage/img/1.jpg);"> 
+<div class="wrapsection">
+	<div class="parallax-overlay" style="background-color: #01b0d1;opacity:0.9;"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<!-- Title -->
+				<div class="maintitle">
+					<h3 class="section-title"><strong>LOG MASUK <span class="lighter">SEBAGAI</span></strong> STAF ILKKM</h3>
+
+
+					<center><div class="sufee-login d-flex align-content-center flex-wrap">
+				        <div class="container">
+				            <div class="login-content">
+				                <div class="login-logo">
+				                    <a href="index.html">
+				                        <img class="align-content" src="" alt="">
+				                    </a>
+				                </div>
+				                <div class="login-form">
+				                    <form>
+				                        <div class="form-group">
+				                            <label>KATANAMA</label>
+				                            <input type="email" class="form-control" placeholder="">
+				                        </div>
+				                        <div class="form-group">
+				                            <label>KATALALUAN</label>
+				                            <input type="password" class="form-control" placeholder="">
+				                        </div>
+
+				                       <a href='{{asset("admin.home")}}' target="_blank"> Log masuk </a>
+				                        <div class="checkbox">
+				                            <label>
+				                                <input type="checkbox"> Remember Me
+				                            </label>
+				                        </div>
+				                    </form>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+   			 	</center>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+</section>
+
+<!-- FAQ
+	================================================== -->
+ <section id="student" class="section">
+<div class="wrapsection">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 sol-sm-12">
+				<div class="maintitle">
+					<h3 class="section-title">LOG MASUK<span class="wow bounceInRight"> SEBAGAI PELATIH</span></h3>
+					
+					<center><div class="sufee-login d-flex align-content-center flex-wrap">
+				        <div class="container">
+				            <div class="login-content">
+				                <div class="login-logo">
+				                    <a href="index.html">
+				                        <img class="align-content" src="" alt="">
+				                    </a>
+				                </div>
+				                <div class="login-form">
+				                    <form>
+				                        <div class="form-group">
+				                            <label>KATANAMA</label>
+				                            <input type="email" class="form-control" placeholder="">
+				                        </div>
+				                        <div class="form-group">
+				                            <label>KATALALUAN</label>
+				                            <input type="password" class="form-control" placeholder="">
+				                        </div>
+
+				                         <a href='{{asset("student.home")}}' target="_blank"> Log masuk </a>
+				                        <div class="checkbox">
+				                            <label>
+				                                <input type="checkbox"> Remember Me
+				                            </label>
+				                         </div>
+				                    </form>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+   			 	</center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+<!-- Random
+	================================================== -->
+<!-- <section class="whitecolor parallax section" style="background-image: url(http://themepush.com/demo/runcharity/frontpage/img/5.jpg);"> 
+<div class="wrapsection">
+	<div class="container">
+	<div class="row">
+		<div class="col-md-12 sol-sm-12">
+			<div class="maintitle">
+				<h3 class="section-title justtitle">Charity <span class="wow flipInX">&amp; love</span> are the same</h3>
+				<p class="lead bottom0">
+					 Fusce fermentum lectus id turpis suscipit, ut egestas magna volutpat. Donec a purus tellus. Nunc ut dolor eu est gravida auctor quis vitae.
+				</p>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+</section> -->
+<!-- Contact
+	================================================== -->
+<section id="contactarea" class="parallax section" style="background-image: url(http://themepush.com/demo/runcharity/frontpage/img/map.png);">
+<div class="wrapsection">
+	<div class="parallax-overlay" style="background-color: #1cbb9b;opacity:0.95;"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="maintitle">
+					<h3 class="section-title">Get in Touch</h3>
+					<p class="lead">
+						If you have questions about our events, don't hesitate to send us your message. This is a real working contact form, test it!
+					</p>
+				</div>
+				<form id="contact" name="contact" method="post" class="text-left">
+					<fieldset>
+						<div class="row">
+							<div class="col-md-4 wow fadeIn animated animated" data-wow-delay="0.1s" data-wow-duration="2s">
+								<label for="name">Name<span class="required">*</span></label>
+								<input type="text" name="name" id="name" size="30" value="" required/>
+							</div>
+							<div class="col-md-4 wow fadeIn animated" data-wow-delay="0.3s" data-wow-duration="2s">
+								<label for="email">Email<span class="required">*</span></label>
+								<input type="text" name="email" id="email" size="30" value="" required/>
+							</div>
+							<div class="col-md-4 wow fadeIn animated" data-wow-delay="0.3s" data-wow-duration="2s">
+								<label for="phone">Phone</label>
+								<input type="text" name="phone" id="phone" size="30" value=""/>
+							</div>
+						</div>
+						<div class="wow fadeIn animated" data-wow-delay="0.3s" data-wow-duration="1.5" style="margin-top:15px;">
+							<label for="message">Message<span class="required">*</span></label>
+							<textarea name="message" id="message" required></textarea>
+						</div>
+						<div class="wow fadeIn animated" data-wow-delay="0.3" data-wow-duration="1.5s">
+							<input id="submit" type="submit" name="submit" value="Send"/>
+						</div>
+					</fieldset>
+				</form>
+				<div id="success">		
+					<p class="contactalert">
+						Your message was sent succssfully! I will be in touch as soon as I can.
+					</p>			
+				</div>
+				<div id="error">			
+					<p class="contactalert">
+						 Something went wrong, try refreshing and submitting the form again.
+					</p>			
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+<!-- Credits 
+=============================================== -->
+<section id="credits" class="text-center">
+	<span class="social wow zoomIn">
+	<a href="#"><i class="fa fa-facebook"></i></a>
+	<a href="#"><i class="fa fa-twitter"></i></a>
+	<a href="#"><i class="fa fa-skype"></i></a>
+	<a href="#"><i class="fa fa-linkedin"></i></a>
+	<a href="#"><i class="fa fa-pinterest"></i></a>
+	<a href="#"><i class="fa fa-google-plus"></i></a>
+	</span><br/>
+	Copyright &copy; <a href="#">Your Agency</a> 
+	<br/>Template By <i class="fa fa-heart"></i> WowThemes.Net
+</section>
+<!-- Bootstrap core JavaScript
+	================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="frontpage/js/jquery.min.js"></script>
+<script src="frontpage/js/bootstrap.min.js"></script>
+<script src="frontpage/js/waypoints.min.js"></script>
+<script src="frontpage/js/jquery.scrollTo.min.js"></script>
+<script src="frontpage/js/jquery.localScroll.min.js"></script>
+<script src="frontpage/js/jquery.magnific-popup.min.js"></script>
+<script src="frontpage/js/validate.js"></script>
+<script src="frontpage/js/common.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontpage.index');
+});
+
+
+
+Route::get('/admin.home', function () {
+    return view('admin.index');
+});
+
+Route::get('/student.home', function () {
+    return view('student.shapely.index');
+});
